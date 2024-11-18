8febc33f45fd3dbab9a00330d9651b8dc39a55c3
Noisk8 <juansk8jaramillo@gmail.com>
2024-11-18 00:28:18 -0500

    refactor + 1

--- src/Paginas/Notas.js
+++ src/Paginas/Notas.js
@@ -6,7 +6,7 @@ const Notas = () => {
     <div className='p-8'>
       <h1 className='font-bold text-4xl pb-4 pt-4 '>Inspiración</h1>
 
-      <p>Estas soon algunas notas</p>
+      <p>Este proyecto está inspirado en el programa de Alejandro Pino Calad</p>
 
       {/* <Contador /> */}
     </div>
